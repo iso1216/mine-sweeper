7ba0d34370aaa67c3bf2aefdc3053ece3dd40075
iso1216 <taishi2002i@gmail.com>
2023-06-29 23:13:12 +0900

    上下中央ぞろえ クリックしたときにその場所が表示される機能のついか

--- src/Board.js
+++ src/Board.js
@@ -1,41 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      board: Array(this.props.wide**2).fill(0),
-    };
-  }
+const Board = ({ wide, board }) => {
+  const [boardState, setBoardState] = useState(Array(wide ** 2).fill(null));
 
-  handleClick(i){
-    const board=this.state.board.slice();
-    board[i]=this.props.board[i];
-    this.setState({
-      board: board,
-    })
-  }
-  renderBox(i) {
+  const handleClick = (i) => {
+    const newBoard = boardState.slice();
+    newBoard[i] = board[i];
+    setBoardState(newBoard);
+  };
+
+  const renderBox = (i) => {
     return (
-      <button className="box" onClick={this.handleClick(i)}>{this.state.board[i]}</button>
+      <button className="box" onClick={() => handleClick(i)}>
+        {boardState[i]}
+      </button>
     );
-  }
+  };
 
-  render() {
-    const edge = [];
-    for (let index = 0; index < this.props.wide; index++) {
-      edge.push(index);
-    }
-    return (
-      <div>
-        {edge.map((index) => (
-          <div className="board-row" key={index}>
-            {edge.map((number) => this.renderBox(number + this.props.wide * index))}
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+  const edge = Array.from({ length: wide }, (_, index) => index);
+
+  return (
+    <div>
+      {edge.map((index) => (
+        <div className="board-row" key={index}>
+          {edge.map((number) => renderBox(number + wide * index))}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Board;
--- src/Game.js
+++ src/Game.js
@@ -1,34 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
 
-class Game extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      board: Array(this.props.wide**2).fill(0),
-      set: false,
-    }
-  }
+const Game = ({ wide, bombs }) => {
+  const [board, setBoard] = useState(Array(wide ** 2).fill(0));
+  const [isSet, setIsSet] = useState(false);
 
-  checkBombs(board){
+  const checkBombs = (board) => {
     for (let index = 0; index < board.length; index++) {
-      if (board[index] === "bombs"){
-        for (let i = -1; i <= 1; i++){
-          for (let j = -1; j <=1; j++){
-            if (index%this.props.wide===0 && j===-1) continue;
-            if (index%this.props.wide===this.props.wide-1 && j===1) continue;
-            if (board[index+j+(8*i)]!=="bombs") board[index+j+(8*i)]++;
+      if (board[index] === "bombs") {
+        for (let i = -1; i <= 1; i++) {
+          for (let j = -1; j <= 1; j++) {
+            if (index % wide === 0 && j === -1) continue;
+            if (index % wide === wide - 1 && j === 1) continue;
+            if (board[index + j + wide * i] !== "bombs") board[index + j + wide * i]++;
           }
         }
       }
     }
     return board;
-  }
+  };
 
-  generateRandomNumbers(range, count, board) {
+  const generateRandomNumbers = (range, count, board) => {
     const numbers = Array.from({ length: range }, (_, index) => index + 1);
     const randomNumbers = [];
-  
+
     for (let i = 0; i < count; i++) {
       const randomIndex = Math.floor(Math.random() * numbers.length);
       const randomNumber = numbers[randomIndex];
@@ -39,35 +34,21 @@ class Game extends React.Component {
       board[randomNumbers[index]] = "bombs";
     }
     return board;
-  }
-  
+  };
 
-  setBombs(){
-    const board = this.state.board.slice();
-    const bombsBoard = this.generateRandomNumbers(this.props.wide**2,this.props.bombs,board);
-    const startBoard = this.checkBombs(bombsBoard);
-    this.setState({
-      board:startBoard,
-      set: true,
-    });
-  }
+  const setBombs = () => {
+    const newBoard = board.slice();
+    const bombsBoard = generateRandomNumbers(wide ** 2, bombs, newBoard);
+    const startBoard = checkBombs(bombsBoard);
+    setBoard(startBoard);
+    setIsSet(true);
+  };
 
-  ViewBoard(){
-    return(
-        <Board
-          wide = {this.props.wide}
-          board={this.state.board}
-        />
-    );
-  }
+  const viewBoard = () => {
+    return <Board wide={wide} board={board} />;
+  };
 
-  render(){
-    return(
-      <div>
-        {this.state.set ? this.ViewBoard() : this.setBombs()}
-      </div>
-    );
-  }
-}
+  return <div>{isSet ? viewBoard() : setBombs()}</div>;
+};
 
 export default Game;
